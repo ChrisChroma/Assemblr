a70b0f5ab621275b04f85dff4fecf98c96b21b68
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-06-20 14:46:01 -0700

    urls/views set up for crud ops on posts--templates not made

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Post, Student, Message
+
+
 
 # Create your views here.
+
+class PostCreate(CreateView):
+  model = Post
+  fields = ['title', 'content', 'date']
+
+class PostUpdate(UpdateView):
+  model = Post
+  fields = ['title', 'content', 'date']
+
+class PostDelete(DeleteView):
+  model = Post
+  success_url = '/posts/'
+
+
 def home(request):
   return render(request, 'home.html')
 
@@ -8,5 +27,9 @@ def about(request):
   return render(request, 'about.html')
 
 def posts_index(request):
-  posts = Cat.objects.all()
+  posts = Post.objects.all()
   return render(request, 'posts/index.html', { 'posts': posts })
+
+def posts_detail(request):
+    post = Post.objects.get(request, post_id)
+    return render(request, 'posts/detail.html')
