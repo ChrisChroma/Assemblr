c08266f0095927689a1e2a003e0c0b822dc5f9ba
Samuel Trahan <samtrahan91@gmail.com>
2020-06-21 18:49:01 -0500

    add message form

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
+
+    path('posts/<int:post_id>/add_message/', views.add_message, name='add_message'),
 ]
