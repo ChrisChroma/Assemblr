a45c9b0e5c958a4f18f2427c2ddfde55ca01a9a7
ChrisChroma <chrischroma1030@gmail.com>
2020-06-26 12:52:06 -0500

    fixing buttons

--- main_app/views.py
+++ main_app/views.py
@@ -5,12 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-
-
-# from django.views.generic.edit import CreateView, UpdateView, DeleteView
-# from django.views.generic import ListView, DetailView
-
-
 from .models import Post, Student, Message
 from .forms import MessageForm, StudentForm
 
@@ -24,19 +18,8 @@ class PostCreate(LoginRequiredMixin, CreateView):
     success_url = '/posts/'
 
     def form_valid(self, form):
-    # Assign the logged in user (self.request.user)
-        form.instance.user = self.request.user  # form.instance is the cat
-        # Let the CreateView do its job as usual
+        form.instance.user = self.request.user
         return super().form_valid(form)
-
-# class MessageCreate(CreateView):
-#     model = Message
-#     fields = ['title', 'content']
-#     def form_valid(self, form):
-#     # Assign the logged in user (self.request.user)
-#         form.instance.student = self.request.user  # form.instance is the cat
-#         # Let the CreateView do its job as usual
-#         return super().form_valid(form)
 
 
 
@@ -71,7 +54,6 @@ class MessageDelete(LoginRequiredMixin, DeleteView):
 
 # ========= Functions ===================
 
-@login_required
 def home(request):
     return render(request, 'home.html')
 
@@ -94,12 +76,6 @@ def posts_detail(request, post_id):
     message_form = MessageForm()
     return render(request, 'posts/detail.html', {'post': post, 'message_form': message_form})
 
-
-# def message_details(request, post_id, message_id):
-#     post = Post.objects.get(id=post_id)
-#     message = Message.objects.get(id=message_id)
-#     return render(request, 'yourtemplate', {'post': post, 'message': message})
-
 @login_required
 def add_message(request, post_id):
     form = MessageForm(request.POST)
@@ -113,18 +89,13 @@ def add_message(request, post_id):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
